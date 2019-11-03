036e9ce6817c4e8f9f58e17a8c3cc1086876b7d3
Ross Ragsdale <ross.ragsdale@gmail.com>
2019-11-03 01:52:21 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -78,7 +78,5 @@ for d in */ ; do
     cd $d && npm install --no-package-lock && cd ..
 done
 
-npm init -y
-npm install aws-sam-local
-node_modules/aws-sam-local/node_modules/.bin/sam package --template-file $TEMPLATE --output-template-file serverless-output.yaml --s3-bucket $AWS_DEPLOY_BUCKET $AWS_BUCKET_PREFIX $FORCE_UPLOAD $USE_JSON
-node_modules/aws-sam-local/node_modules/.bin/sam deploy --template-file serverless-output.yaml --stack-name $AWS_STACK_NAME $CAPABILITIES $PARAMETER_OVERRIDES
+nsam package --template-file $TEMPLATE --output-template-file serverless-output.yaml --s3-bucket $AWS_DEPLOY_BUCKET $AWS_BUCKET_PREFIX $FORCE_UPLOAD $USE_JSON
+sam deploy --template-file serverless-output.yaml --stack-name $AWS_STACK_NAME $CAPABILITIES $PARAMETER_OVERRIDES
