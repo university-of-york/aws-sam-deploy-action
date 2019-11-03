290c20d6340f57e58c9baa8cd866df669cee72c9
Ross Ragsdale <ross.ragsdale@gmail.com>
2019-11-03 01:01:27 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -78,5 +78,5 @@ for d in */ ; do
     cd $d && npm install --no-package-lock && cd ..
 done
 
-nsam package --template-file $TEMPLATE --output-template-file serverless-output.yaml --s3-bucket $AWS_DEPLOY_BUCKET $AWS_BUCKET_PREFIX $FORCE_UPLOAD $USE_JSON
+sam package --template-file $TEMPLATE --output-template-file serverless-output.yaml --s3-bucket $AWS_DEPLOY_BUCKET $AWS_BUCKET_PREFIX $FORCE_UPLOAD $USE_JSON
 sam deploy --template-file serverless-output.yaml --stack-name $AWS_STACK_NAME $CAPABILITIES $PARAMETER_OVERRIDES
